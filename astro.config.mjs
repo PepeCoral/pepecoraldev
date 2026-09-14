ce62eec2532cb928a02bb3c4f15a626c66261795
Pepe Coral <coritochico@gmail.com>
2026-09-14 18:54:27 +0200

    feat: :sparkles: added ci for deplying

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    site: "https://pepecoral.dev",
     vite: {
         plugins: [tailwindcss()],
     },
